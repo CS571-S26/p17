8d60e4fc14fd0a3fec4a19bc7e1d511ecb6c7e85
Saahil Khatkhate <saahil.khatkhate@gmail.com>
2026-03-17 13:52:07 -0500

    standardize semicolons

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter } from 'react-router-dom';
 
